05021ea71109599828a354220cae0d5333d64dd7
Alexandre Booh Louha <ablbooh0@gmail.com>
2025-11-11 16:52:27 -0500

    Ajoute des autres features de service

--- backend/src/controllers/serviceController.js
+++ backend/src/controllers/serviceController.js
@@ -57,3 +57,54 @@ export const deleteService = async (req, res) => {
     res.status(500).json({ message: "Server error while deleting service" });
   }
 };
+
+export const getMyServices = async (req, res) => {
+    try{
+        const result = await pool.query(
+            `SELECT * FROM services WHERE user_id = $1 ORDER BY created_at DESC`,
+            [req.user.id]
+        );
+        res.json(result.rows);
+    } catch (err) {
+        console.error(err);
+        res.status(500).json({ message: "Server error while fetching your services" });
+    }
+}
+
+
+export const updateService = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description, category, price, location } = req.body;
+
+    const check = await pool.query(
+      `SELECT * FROM services WHERE id = $1 AND user_id = $2`,
+      [id, req.user.id]
+    );
+
+    if (check.rows.length === 0) {
+      return res.status(403).json({ message: "You can't update this service" });
+    }
+
+    const existing = check.rows[0];
+    const updated = await pool.query(
+      `UPDATE services 
+       SET title = $1, description = $2, category = $3, price = $4, location = $5
+       WHERE id = $6
+       RETURNING *`,
+      [
+        title || existing.title,
+        description || existing.description,
+        category || existing.category,
+        price || existing.price,
+        location || existing.location,
+        id,
+      ]
+    );
+
+    res.json(updated.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ message: "Server error while updating service" });
+  }
+};
--- backend/src/routes/serviceRoutes.js
+++ backend/src/routes/serviceRoutes.js
@@ -1,11 +1,13 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
-import { createService, getAllServices, deleteService } from "../controllers/serviceController.js";
+import { createService, getAllServices, deleteService, getMyServices, updateService  } from "../controllers/serviceController.js";
 
 const router = express.Router();
 
 router.post("/", protect, createService);
 router.get("/", getAllServices);
+router.get("/my-services", protect, getMyServices);
+router.put("/:id", protect, updateService);
 router.delete("/:id", protect, deleteService);
 
 export default router;
